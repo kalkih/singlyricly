c0dc381d0e51b617e521dd83f720cb68ec0cd80d
Karl Kihlström <mrkihlstrom@gmail.com>
2019-10-04 16:50:58 +0200

    :lipstick: Adjust theme variables

--- src/utils/color.js
+++ src/utils/color.js
@@ -1,5 +1,5 @@
 const getHsl = (arr, per = 0) => {
-  const l = Math.max(10, Math.min((arr[2] * 100 - per), 100))
+  const l = Math.max(15, Math.min((arr[2] * 100 - per), 100))
   return `hsl(${arr[0] * 360}, ${arr[1] * 100}%, ${l}%)`
 }
 
