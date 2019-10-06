010d79e8c9440b8b30398aca86a6745b9e1456a1
Karl Kihlström <mrkihlstrom@gmail.com>
2019-10-06 18:06:51 +0200

    :lipstick: Changed dark color min brightness

--- src/utils/color.js
+++ src/utils/color.js
@@ -1,6 +1,11 @@
+const MIN = 10
+const MAX = 100
+const DEGREES = 360
+const MULTI = 100
+
 const getHsl = (arr, per = 0) => {
-  const l = Math.max(15, Math.min((arr[2] * 100 - per), 100))
-  return `hsl(${arr[0] * 360}, ${arr[1] * 100}%, ${l}%)`
+  const l = Math.max(MIN, Math.min((arr[2] * MULTI - per), MAX))
+  return `hsl(${arr[0] * DEGREES}, ${arr[1] * MULTI}%, ${l}%)`
 }
 
 export default getHsl
