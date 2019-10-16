ac3c800390b4f226cb2364413f240a09f6665637
Karl Kihlström <mrkihlstrom@gmail.com>
2019-10-16 18:35:50 +0200

    :wrench: Added support for correct branch auth redirects

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,8 +1,14 @@
 import request from './request'
 
+const subdomain = process.env.VUE_APP_SUBDOMAIN
+
 export default {
   async getAuthURL () {
-    const req = await request.get('/')
+    const req = await request.get('/', {
+      ...(subdomain && {
+        params: { sub: subdomain },
+      }),
+    })
     return req.status === 200 ? req.data.url : null
   },
   async refreshToken (token) {
