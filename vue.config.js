0f35eac378139346d516f9aa7e173be65e2418c6
Karl Kihlström <mrkihlstrom@gmail.com>
2021-10-08 23:45:14 +0200

    :arrow_up: Upgrade dependencies

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
     svgRule.uses.clear()
     svgRule
       .use('vue-loader')
-      .loader('vue-loader-v16')
+      .loader('vue-loader')
       .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
